775a95f1322050d732b81880741d4724eb31a33b
Aaron Unger <aaron.unger@gmail.com>
2013-07-25 15:58:01 -0700

    Migrations
    The monkey patch in Engine allows the portal to use this engine's
    migrations without needing to import and manage them in its own
    codebase.

--- lib/cc_portal_wordpress_integration/engine.rb
+++ lib/cc_portal_wordpress_integration/engine.rb
@@ -1,4 +1,9 @@
 module CcPortalWordpressIntegration
   class Engine < ::Rails::Engine
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      end
+    end
   end
 end
